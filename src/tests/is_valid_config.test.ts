da4993cff4f2da96cbbb1c1b19a7447175cedc45
Shuffle21 <>
2023-01-18 02:37:40 +0200

    Added test for is_valid_config module

---
+++ src/tests/is_valid_config.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from '@jest/globals';
+
+import { is_valid_config } from '../utils/is_valid_config';
+
+
+describe("is_valid_config module", () => {
+    test("Return true if the config is valid", () => {
+        expect(is_valid_config({targetDirectory: ""} as any)).toBe(false);
+    })
+    test("Return true if the config is valid", () => {
+        expect(is_valid_config({
+                targetUsername: "string",
+                targetDirectory: "string",
+                recordLength: "string",
+                videoFormat: "mp4",
+                serverPort: 3000
+        })).toBe(true);
+    })
+})
