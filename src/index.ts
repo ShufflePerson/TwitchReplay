37096edc4ef014d12f882d3840a62cf8f79b86d4
Shuffle21 <>
2023-01-20 16:53:21 +0200

    Mostly for testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
 console.clear();
+
+
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
+if (process.env.ENVIRONMENT === "development") dotenv.config({ path: '.env.dev' });
+
+
+
+import logging from "./logging/logging";
+import client from "./twitch/modules/client";
+import globals from './twitch/globals';
+import cap_text from "./utils/cap_text";
+
+
+
+(async () => {
+
+    logging.init();
+
+    globals.channel_name = "xqc";
+    let stream_title: string = await client.get_stream_title();
+
+    logging.get_logger().info(`Stream title: ${cap_text(stream_title)}`);
+
+    await client.set_playback_access_token();
+
+    await client.get_device_id();
+
+
+    console.log(globals);
+})();
+
