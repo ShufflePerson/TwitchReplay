302ee6b3e81e5df265c5695f7787b6d486995bf6
Shuffle21 <>
2023-01-20 16:43:25 +0200

    Added a `init` function to support DEBUG level

--- src/logging/logging.ts
+++ src/logging/logging.ts
@@ -3,8 +3,13 @@ import { Logger, ConsoleHandler, LogLevel } from "logging-library";
 
 
 namespace logging {
-    export const logger = new Logger()
+    export let logger = new Logger()
         .addHandler(new ConsoleHandler(LogLevel.INFO))
+
+    export function init() {
+        if (process.env.ENVIRONMENT === "development")
+            logger = new Logger().addHandler(new ConsoleHandler(LogLevel.DEBUG))
+    }
 }
 
 export default logging;
