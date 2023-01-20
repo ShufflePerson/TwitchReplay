5385e928520103d0560792e7ff5dd94b825b925d
Shuffle21 <>
2023-01-20 16:52:32 +0200

    Changed the way logger export works

--- src/logging/logging.ts
+++ src/logging/logging.ts
@@ -3,12 +3,16 @@ import { Logger, ConsoleHandler, LogLevel } from "logging-library";
 
 
 namespace logging {
-    export let logger = new Logger()
-        .addHandler(new ConsoleHandler(LogLevel.INFO))
+    let logger = new Logger()
+        .addHandler(new ConsoleHandler([LogLevel.DEBUG, LogLevel.INFO]))
 
     export function init() {
         if (process.env.ENVIRONMENT === "development")
-            logger = new Logger().addHandler(new ConsoleHandler(LogLevel.DEBUG))
+            logger = new Logger().addHandler(new ConsoleHandler([LogLevel.DEBUG, LogLevel.INFO]))
+    }
+
+    export function get_logger() {
+        return logger;
     }
 }
 
