af5f085330f83d5d8e26f8ccfea0798e0ba7cdb2
Shuffle21 <>
2023-01-20 17:04:36 +0200

    Added `get_playlist_data` function

--- src/twitch/modules/client.ts
+++ src/twitch/modules/client.ts
@@ -143,6 +143,17 @@ namespace client {
         })
     }
 
+
+    export function get_playlist_data(channel_name: string = globals.channel_name): Promise<string> {
+        return new Promise(async (resolve, reject) => {
+            
+            let resp = await axios.get(`
+                https://usher.ttvnw.net/api/channel/hls/${channel_name}.m3u8?allow_source=true&sig=${globals.signature}&token=${globals.token} `)
+
+            resolve(resp.data);
+        });
+    }
+
 }
 
 export default client;
