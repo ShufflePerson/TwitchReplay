a7af0e77c36ec575c031fcb98679dfe63372908d
Shuffle21 <>
2023-01-21 22:57:21 +0200

    Implemented a custom logging handler

--- src/logging/logging.ts
+++ src/logging/logging.ts
@@ -1,14 +1,42 @@
-import { Logger, ConsoleHandler, LogLevel } from "logging-library";
+import { Logger, ConsoleHandler, LogLevel, BaseHandler } from "logging-library";
+import { LogRecord } from "logging-library/lib/types/log-record";
 
 
 
-namespace logging {
+//custom handler
+
+class log_handler extends BaseHandler {
+    constructor(level: LogLevel | LogLevel[]) {
+        super(level);
+    }
+
+    protected log(record: LogRecord) {
+        let msg = record.message;
+        switch (record.level) {
+            case LogLevel.DEBUG:
+                msg = `\x1b[36m${msg}\x1b[0m`;
+                break;
+            case LogLevel.INFO:
+                msg = `\x1b[32m${msg}\x1b[0m`;
+                break;
+            case LogLevel.WARNING:
+                msg = `\x1b[33m${msg}\x1b[0m`;
+                break;
+            case LogLevel.ERROR:
+                msg = `\x1b[31m${msg}\x1b[0m`;
+                break;
+            case LogLevel.CRITICAL:
+                msg = `\x1b[31m${msg}\x1b[0m`;
+                break;
+        }
+
+        console.log(msg);
+    }
+} namespace logging {
     let logger = new Logger()
-        .addHandler(new ConsoleHandler([LogLevel.DEBUG, LogLevel.INFO]))
+        .addHandler(new log_handler([LogLevel.DEBUG, LogLevel.INFO, LogLevel.WARNING, LogLevel.ERROR, LogLevel.CRITICAL]))
 
     export function init() {
-        if (process.env.ENVIRONMENT === "development")
-            logger = new Logger().addHandler(new ConsoleHandler([LogLevel.DEBUG, LogLevel.INFO]))
     }
 
     export function get_logger() {
