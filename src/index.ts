4476e770cd4c3b3d794cb8b79627d825bd3c9cfc
Shuffle21 <>
2023-01-21 23:10:37 +0200

    Proper `main` function

--- src/index.ts
+++ src/index.ts
@@ -11,23 +11,42 @@ import logging from "./logging/logging";
 import client from "./twitch/modules/client";
 import globals from './twitch/globals';
 import cap_text from "./utils/cap_text";
+import { twitch } from './twitch/twitch';
+import readline from 'readline';
+import clipper from './clipper/clipper';
 
+
+readline.emitKeypressEvents(process.stdin);
 
 
 (async () => {
 
+    let logger = logging.get_logger().withContext("main");
+
     logging.init();
 
-    globals.channel_name = "xqc";
+    let twitch_init: boolean = await twitch.initlize();
+    if (!twitch_init) {
+        logger.error("Failed to initialize. Please check the log file for more information.");
+        return;
+    }
+
     let stream_title: string = await client.get_stream_title();
 
     logging.get_logger().info(`Stream title: ${cap_text(stream_title)}`);
-
-    await client.set_playback_access_token();
-
-    //await client.get_device_id();
+    
+    await clipper.initlize();
 
 
-    console.log(globals);
+    logger.info("Attempting to start capture");
+    
+    let capture_started: boolean = await clipper.start_capture();
+    if (!capture_started) {
+        logger.error("Failed to start capture. Please check the log file for more information.");
+        return;
+    }
+
 })();
+
+
 
