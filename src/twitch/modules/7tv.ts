828c1ba1732612fb2c52199290d041f300558cb9
Shuffle21 <>
2023-01-27 14:50:33 +0200

    Fixed bugs & Added better caching

--- src/twitch/modules/7tv.ts
+++ src/twitch/modules/7tv.ts
@@ -15,7 +15,7 @@ namespace seventv {
     function resolve_when_cached(identifier: {
         id?: string,
         name?: string
-    }, resolve: Function) {
+    }, resolve: Function, resolve_null_on_fail: boolean = false) {
         let emote = cached_emotes.find((emote: t_seventv_emote) => {
             if (identifier.id) {
                 return emote.id == identifier.id;
@@ -26,6 +26,8 @@ namespace seventv {
 
         if (emote) {
             resolve(emote);
+        } else if (resolve_null_on_fail) {
+            resolve(null);
         }
     }
 
@@ -33,20 +35,29 @@ namespace seventv {
         cached_emotes.push(emote);
     }
 
-    export function get_global_emotes(): Promise<t_seventv_emote> {
+    export function get_global_emotes(): Promise<t_seventv_emote[]> {
         return new Promise(async (resolve, reject) => {
             let resp = await axios.get(endpoint + "emotes/global");
-            resolve(resp.data);
-            add_cache_emote(resp.data)
+            let data = resp.data as t_seventv_emote[];
+            resolve(data);
+
+            data.forEach((emote: t_seventv_emote) => {
+                add_cache_emote(emote);
+            });
         })
     }
 
-    export function get_channel_emotes(channel_id: string): Promise<t_seventv_emote> {
+    export function get_channel_emotes(channel_id: string): Promise<t_seventv_emote[]> {
         return new Promise(async (resolve, reject) => {
-            resolve_when_cached({ id: channel_id }, resolve)
-            let resp = await axios.get(endpoint + `channels/${channel_id}/emotes`);
-            resolve(resp.data);
-            add_cache_emote(resp.data)
+            let resp = await axios.get(endpoint + `users/${channel_id}/emotes`);
+
+            let data = resp.data as t_seventv_emote[];
+
+            resolve(data);
+
+            data.forEach((emote: t_seventv_emote) => {
+                add_cache_emote(emote);
+            })
         })
     }
 
@@ -60,21 +71,7 @@ namespace seventv {
 
     export function get_emote_by_name(emote_name: string): Promise<t_seventv_emote | null> {
         return new Promise(async (resolve, reject) => {
-            logger.debug(`Getting emote by name ${emote_name}`)
-            if (!not_an_emote.includes(emote_name)) {
-                resolve(null);
-            }
-            try {
-                resolve_when_cached({ name: emote_name }, resolve)
-                let resp = await axios.get(endpoint + `emotes/name/${emote_name}`);
-                resolve(resp.data);
-                add_cache_emote(resp.data)
-                logger.debug(`Got emote by name ${emote_name}`)
-            } catch (ex) {
-                logger.debug(`Failed to get emote by name ${emote_name}`)
-                not_an_emote.push(emote_name);
-            }
-
+            resolve_when_cached({ name: emote_name }, resolve, true)
         })
     }
 }
