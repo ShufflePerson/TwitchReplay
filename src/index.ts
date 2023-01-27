940b787eb6ae96d2ae7fe027fb39271fe7ad2b5e
Shuffle21 <>
2023-01-27 13:48:29 +0200

    Added test clipping & cleanup on quit

--- src/index.ts
+++ src/index.ts
@@ -25,11 +25,15 @@ readline.emitKeypressEvents(process.stdin);
 
     logging.init();
 
+
     let twitch_init: boolean = await twitch.initlize();
     if (!twitch_init) {
         logger.error("Failed to initialize. Please check the log file for more information.");
         return;
     }
+
+
+    await client.get_device_id();
 
     let stream_title: string = await client.get_stream_title();
 
@@ -40,13 +44,34 @@ readline.emitKeypressEvents(process.stdin);
 
     logger.info("Attempting to start capture");
     
-    let capture_started: boolean = await clipper.start_capture(true);
+    let capture_started: boolean = await clipper.start_capture(false);
     if (!capture_started) {
         logger.error("Failed to start capture. Please check the log file for more information.");
         return;
     }
 
+
+    setTimeout(async () => {
+        let clipped: boolean = await clipper.clip(10);
+        if (!clipped) {
+            logger.error("Failed to clip. Please check the log file for more information.");
+            return;
+        }
+
+        logger.info("Clip successful");
+    }, 12000);
+
+
 })();
 
 
 
+
+
+process.on('exit', async () => {
+    clipper.clean_up();
+});
+
+process.on('SIGINT', async () => {
+    clipper.clean_up();
+});
