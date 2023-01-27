fce7e52e8cd9514e50ee2e78b3c8698479a55c08
Shuffle21 <>
2023-01-27 13:48:08 +0200

    Recoded `clip` function, Added a `clean_up` function

--- src/clipper/clipper.ts
+++ src/clipper/clipper.ts
@@ -1,14 +1,15 @@
 
 import logging from '../logging/logging';
 import { execSync } from 'child_process';
-import fs from 'fs';
+import fs, { writeFileSync } from 'fs';
 import is_linux from '../utils/is_linux';
 import ffmpeg from 'fluent-ffmpeg';
 import client from './../twitch/modules/client';
 import globals from './../twitch/globals';
 import get_config from './../utils/get_config';
 import { resolve } from 'path';
-import cli_spinners from 'cli-spinners'; 
+import cli_spinners from 'cli-spinners';
+import is_dev from '../utils/is_dev';
 
 namespace clipper {
     let logger = logging.get_logger().withContext("clipper");
@@ -60,7 +61,7 @@ namespace clipper {
 
         return new Promise(async (resolve, reject) => {
             logger.info("Starting Capture");
-            
+
             let ffmpeg_options: string[] = [];
 
             ffmpeg_options.push("-c:v");
@@ -81,7 +82,7 @@ namespace clipper {
             ffmpeg_options.push("./cache/buffer/playlist.m3u8");
             ffmpeg_options.push("-segment_list_flags");
             ffmpeg_options.push("+live");
-            ffmpeg_options.push("./cache/buffer/segment-%03d.ts");
+            ffmpeg_options.push("./cache/buffer/%d.ts");
             ffmpeg_options.push("-c");
             ffmpeg_options.push("copy");
             ffmpeg_options.push("./cache/" + globals.channel_name + ".ts");
@@ -105,11 +106,12 @@ namespace clipper {
                     if (!is_first_run)
                         resolve(true);
                     else {
+                        let ad_break_time = 0;
                         logger.info("Waiting for ad-break to end")
                         setTimeout(() => {
                             logger.info("Ad-break ended. Continuing capture")
                             resolve(true);
-                        }, 16 * 1000);
+                        }, ad_break_time);
                     }
 
                 }).on("error", function (err) {
@@ -143,28 +145,61 @@ namespace clipper {
     export function clip(time_s: number): Promise<boolean> {
         return new Promise(async (resolve, reject) => {
 
-            let clips_needed: number = Math.floor(time_s / globals.buffer_size);
-            let clips_available: number = fs.readdirSync("./cache/buffer").length;
+            let buffer_needed: number = Math.round(time_s / globals.buffer_size);
+            let buffer_available: number = fs.readdirSync("./cache/buffer").length - 1;
 
-            logger.info("Clips needed: " + clips_needed);
-            logger.info("Clips available: " + clips_available);
+            logger.debug("Buffer needed: " + buffer_needed);
+            logger.debug("Buffer available: " + buffer_available);
 
-            if (clips_available < clips_needed) {
-                logger.warning("Not enough clips available. Capping to " + clips_available * globals.buffer_size + " seconds");
-                clips_needed = clips_available;
-                time_s = clips_available * globals.buffer_size;
+            if (buffer_available < buffer_needed) {
+                logger.warning("Not enough clips available. Capping to " + buffer_available * globals.buffer_size + " seconds");
+                buffer_needed = buffer_available;
+                time_s = buffer_available * globals.buffer_size;
             }
 
             let output_name = `${get_config().targetDirectory}/${globals.channel_name}_${new Date().getTime()}.${get_config().videoFormat}`;
 
             logger.info("Clipping " + time_s + " seconds to " + output_name);
 
-            let ffmpeg_options: string[] = [];
+            let command = ffmpeg();
+            for (let i = buffer_available - buffer_needed; i < buffer_available; i++) {
+                command = command.addInput(`./cache/buffer/${i}.ts`);
+            }
+
+            command.mergeToFile(output_name)
+                .on("start", function (command_line) {
+                    logger.info("Clip started");
+                    logger.debug(command_line);
+                }).on("error", function (err) {
+                    logger.error("An error occurred: " + err.message)
+                    resolve(false);
+                }).on("end", function () {
+                    logger.info("Clip ended");
+                    resolve(true);
+                })
 
 
 
 
         })
+    }
+
+    export function clean_up() {
+        mpeg.kill("SIGKILL");
+        //delete cache
+
+        if (is_dev()) {
+
+            if (is_linux())
+                execSync("rm -rf ./cache");
+            else
+                execSync("rmdir /s /q cache");
+        }
+
+
+        logger.info("Cleaned up");
+
+
     }
 
 }
