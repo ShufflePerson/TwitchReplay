5704a2f087588455dcf41bfe9dd2368e05ff7118
Shuffle21 <>
2023-01-28 00:30:36 +0200

    Removed `recordLength` from the config type

--- src/types/t_config.ts
+++ src/types/t_config.ts
@@ -4,11 +4,10 @@ import { keys } from 'ts-transformer-keys'
 interface t_config {
     targetUsername: string,
     targetDirectory: string,
-    recordLength: string,
     videoFormat: "mp4" | "avi" | "mkv",
     serverPort: number
 }
 
-export const tk_config: Array<keyof t_config> = ["targetUsername", "targetDirectory", "recordLength", "videoFormat", "serverPort"];
+export const tk_config: Array<keyof t_config> = ["targetUsername", "targetDirectory", "videoFormat", "serverPort"];
 
 export { t_config }
