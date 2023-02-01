bafcc285fcd20538b1c297a37c32aa0c7f19200d
Shuffle21 <>
2023-02-01 15:03:32 +0200

    Implemented `send_chromedriver´ and various fixes

--- src/ui_chat/ui_chat.ts
+++ src/ui_chat/ui_chat.ts
@@ -9,17 +9,20 @@ import ffmpeg from 'fluent-ffmpeg';
 import is_linux from '../utils/is_linux';
 import { execSync } from 'child_process';
 import { t_chat_ui_message } from '../types/t_chat_ui_message';
+import http from 'http';
 
 
 ffmpeg.setFfmpegPath("./ffmpeg/bin/ffmpeg.exe");
 ffmpeg.setFfprobePath("./ffmpeg/bin/ffprobe.exe");
 
 
+let delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
 namespace ui_chat {
 
     let driver_build = new Builder()
         .forBrowser('chrome')
-        .setChromeOptions(new Options().windowSize({ width: 430, height: 520 }))
+        .setChromeOptions(new Options().addArguments(`--disable-infobars --disable-gpu --app=http://localhost:${get_config().serverPort}/chat/chat.html`).windowSize({ width: 430, height: 520 }))
     let driver: WebDriver;
 
     let log = logging.get_logger();
@@ -55,11 +58,14 @@ namespace ui_chat {
 
         driver = (await driver_build.build());
 
-        await driver.get(`http://localhost:${get_config().serverPort}/chat/chat.html`);
+
+        await send_chromedriver("Emulation.setDefaultBackgroundColorOverride", {
+            color: { r: 0, g: 0, b: 0, a: 0 }
+        })
 
 
-        setInterval(() => {
-            take_frame()
+
+        for (let i = 0; i < 10; i++) {
             driver.executeScript(get_add_chat_message_script({
                 text: "OMEGALUL so true OMEGALUL",
                 username: "Arthium",
@@ -68,16 +74,73 @@ namespace ui_chat {
                     OMEGALUL: "https://cdn.frankerfacez.com/emoticon/128054/2"
                 },
                 badges: ["https://static-cdn.jtvnw.net/badges/v1/affddbd9-df5d-4c55-935f-d75767585ee9/2"]
-            }));
-        }, 1000)
+            }))
+
+            await delay(300);
+            await take_frame()
+
+
+        }
+
+
+        await driver.quit();
+        await convert_to_video();
 
         log.info("Chat's Frontend Initialized");
     }
+
+
+    function send_chromedriver(cmd: string, params: any): Promise<boolean> {
+
+        return new Promise(async (resolve, reject) => {
+            let session_id = (await (driver.getSession())).getId();
+            let resource = `/session/${session_id}/chromium/send_command_and_get_result`;
+            let url: string = (await (await driver.getCapabilities()).get('goog:chromeOptions')).debuggerAddress;
+            let body = {
+                cmd: cmd,
+                params: params
+            }
+            let options = {
+                hostname: url.split(":")[0],
+                port: url.split(":")[1],
+                path: resource,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': Buffer.byteLength(JSON.stringify(body))
+                }
+            };
+
+
+            let req = http.request(options, (res) => {
+                res.on('data', (data) => {
+                    log.debug(data.toString());
+                });
+                res.on('end', () => {
+                    log.debug("Request ended")
+                    resolve(true);
+                });
+            });
+
+            req.on('error', (e) => {
+                log.error(e);
+                reject(e);
+            });
+
+            req.write(JSON.stringify(body));
+            req.end();
+
+
+        })
+
+    }
+
 
     export async function take_frame() {
         let screenshot = await driver.takeScreenshot();
         writeFileSync(`./cache/chat/${frames_done++}.png`, screenshot, 'base64');
     }
+
     export async function convert_to_video(): Promise<string> {
 
         return new Promise(async (resolve, reject) => {
@@ -87,6 +150,8 @@ namespace ui_chat {
 
             ffmpeg()
                 .addOptions([
+                    "-r", "1",
+                    "-framerate", "1",
                     "-i ./cache/chat/%d.png"
                 ])
                 .output(`./cache/chat/${file_name}`)
@@ -102,7 +167,6 @@ namespace ui_chat {
                     reject(err);
                 }).run();
 
-            log.info("Chat converted to video");
         });
     }
 
