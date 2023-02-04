afadb3deb5ace7a5f26f9245eaa0744be26f0b71
Shuffle21 <>
2023-02-05 01:40:54 +0200

    Added `clip_start: number, clip_end: number` arguments

--- src/utils/get_message_chunks.ts
+++ src/utils/get_message_chunks.ts
@@ -2,11 +2,13 @@ import { t_message } from "../types/t_message";
 
 
 
-export default ((messages: t_message[], messages_per_chunk: number = 5): t_message[][] => {
+export default ((messages: t_message[], clip_start: number, clip_end: number, messages_per_chunk: number = 5): t_message[][] => {
     let chunks: t_message[][] = [];
     let current_chunk: t_message[] = [];
 
     for (let i = 0; i < messages.length; i++) {
+        if (messages[i].time < clip_start || messages[i].time > clip_end)
+            continue;
         if (current_chunk.length >= messages_per_chunk) {
             chunks.push(current_chunk);
             current_chunk = [];
