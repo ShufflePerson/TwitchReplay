908ad2512bf5bc213829898c1c7234546b4ff091
Shuffle21 <>
2023-02-04 18:28:08 +0200

    Partial refactor & recode, implemented puppeteer

--- src/ui_chat/ui_chat.ts
+++ src/ui_chat/ui_chat.ts
@@ -1,7 +1,6 @@
 
 import { existsSync, mkdirSync, writeFileSync } from 'fs';
-import { Builder, By, Key, ThenableWebDriver, until, WebDriver } from 'selenium-webdriver';
-import chrome from 'selenium-webdriver/chrome';
+//import { Builder, By, Key, ThenableWebDriver, until, WebDriver } from 'selenium-webdriver';
 import { Options } from 'selenium-webdriver/chrome';
 import logging from '../logging/logging';
 import get_config from '../utils/get_config';
@@ -15,6 +14,10 @@ import { resolve } from 'path';
 import in_tolerence from '../math/in_tolerence';
 import { video } from '../video/video';
 
+import puppeteer from 'puppeteer';
+import { Browser, Page } from 'puppeteer';
+
+
 
 ffmpeg.setFfmpegPath("./ffmpeg/bin/ffmpeg.exe");
 ffmpeg.setFfprobePath("./ffmpeg/bin/ffprobe.exe");
@@ -23,11 +26,9 @@ ffmpeg.setFfprobePath("./ffmpeg/bin/ffprobe.exe");
 let delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
 namespace ui_chat {
+    let browser: Browser;
+    let page: Page;
 
-    let driver_build = new Builder()
-        .forBrowser('chrome')
-        .setChromeOptions(new Options().addArguments(`--disable-infobars --disable-gpu --app=http://localhost:${get_config().serverPort}/chat/chat.html`).windowSize({ width: 630, height: 620 }))
-    let driver: WebDriver;
 
     let log = logging.get_logger();
     let screenshot_interval: NodeJS.Timeout;
@@ -54,14 +55,13 @@ namespace ui_chat {
         return `add_chat_message(${message_string})`;
     }
 
-    function make_transparent(image: string, template_color = {
+    function make_transparent(image: Buffer, template_color = {
         r: 154,
         g: 154,
         b: 154
     }): Promise<string> {
         return new Promise(async (resolve, reject) => {
-            let image_buffer = Buffer.from(image, 'base64');
-            let image_sharp = sharp(image_buffer);
+            let image_sharp = sharp(image);
             let metadata = await image_sharp.metadata();
             let image_data = await image_sharp.raw().toBuffer();
             let image_data_array = new Uint8ClampedArray(image_data);
@@ -80,9 +80,9 @@ namespace ui_chat {
                 }
             });
 
-            image = (await image_sharp_transparent.png().toBuffer()).toString('base64');
+            let new_image = (await image_sharp_transparent.png().toBuffer()).toString('base64');
 
-            resolve(image);
+            resolve(new_image);
         })
     }
 
@@ -92,11 +92,26 @@ namespace ui_chat {
 
         initlize_folders();
 
-        driver = (await driver_build.build());
+        let url = `http://localhost:${get_config().serverPort}/chat/chat.html`;
+        browser = await puppeteer.launch({
+            headless: false,
+            args: [
+                `--app=${url}`,
+                '--disable-infobars',
+                '--disable-gpu',
+                '--window-size=630,620'
+            ]
+        });
+        page = await browser.newPage();
+        await page.goto(url);
+        await page.setViewport({ width: 630, height: 620 });
+        await page.evaluate(() => (document.body.style.background = 'transparent'))
+
+
 
 
         for (let i = 0; i < 100; i++) {
-            driver.executeScript(get_add_chat_message_script({
+            page.evaluate(get_add_chat_message_script({
                 text: "OMEGALUL so true " + i.toString() + " OMEGALUL",
                 username: "Arthium",
                 color: "#1111EE",
@@ -106,69 +121,26 @@ namespace ui_chat {
                 badges: ["https://static-cdn.jtvnw.net/badges/v1/affddbd9-df5d-4c55-935f-d75767585ee9/2"]
             }))
 
+
             await delay(10);
             await take_frame()
 
 
         }
 
-
-        await driver.quit();
+        await browser.close();
         await convert_to_video("./clips/xqc_1674906587400.mkv");
 
         log.info("Chat's Frontend Initialized");
     }
 
 
-    function send_chromedriver(cmd: string, params: any): Promise<boolean> {
-
-        return new Promise(async (resolve, reject) => {
-            let session_id = (await (driver.getSession())).getId();
-            let resource = `/session/${session_id}/chromium/send_command_and_get_result`;
-            let url: string = (await (await driver.getCapabilities()).get('goog:chromeOptions')).debuggerAddress;
-            let body = {
-                cmd: cmd,
-                params: params
-            }
-            let options = {
-                hostname: url.split(":")[0],
-                port: url.split(":")[1],
-                path: resource,
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Content-Length': Buffer.byteLength(JSON.stringify(body))
-                }
-            };
-
-
-            let req = http.request(options, (res) => {
-                res.on('data', (data) => {
-                    log.debug(data.toString());
-                });
-                res.on('end', () => {
-                    log.debug("Request ended")
-                    resolve(true);
-                });
-            });
-
-            req.on('error', (e) => {
-                log.error(e);
-                reject(e);
-            });
-
-            req.write(JSON.stringify(body));
-            req.end();
-
-
-        })
-
-    }
-
-
     export async function take_frame() {
-        let screenshot = await driver.takeScreenshot();
-        screenshot = await make_transparent(screenshot);
+        const elements = await page.$('body')
+        let screenshot: string = (await elements.screenshot({ encoding: 'base64', omitBackground: true })) as string;
+        //let buffer_screenshot = Buffer.from(screenshot, 'base64');
+
+        //screenshot = await make_transparent(buffer_screenshot);
         writeFileSync(`./cache/chat/${frames_done++}.png`, screenshot, 'base64');
 
         log.debug(`Took frame ${frames_done - 1}`);
