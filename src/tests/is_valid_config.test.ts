7586bec8a3ec87135a695cff7c307d947895c0ba
Shuffle21 <>
2023-02-04 19:09:53 +0200

    Fixed test

--- src/tests/is_valid_config.test.ts
+++ src/tests/is_valid_config.test.ts
@@ -5,15 +5,14 @@ import { is_valid_config } from '../utils/is_valid_config';
 
 describe("is_valid_config module", () => {
     test("Return true if the config is valid", () => {
-        expect(is_valid_config({targetDirectory: ""} as any)).toBe(false);
+        expect(is_valid_config({ targetDirectory: "" } as any)).toBe(false);
     })
     test("Return true if the config is valid", () => {
         expect(is_valid_config({
-                targetUsername: "string",
-                targetDirectory: "string",
-                recordLength: "string",
-                videoFormat: "mp4",
-                serverPort: 3000
+            targetUsername: "string",
+            targetDirectory: "string",
+            videoFormat: "mp4",
+            serverPort: 3000
         })).toBe(true);
     })
 })
