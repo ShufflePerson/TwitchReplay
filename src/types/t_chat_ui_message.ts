8af15d060aea236e64f0ef9ce168053f4fb05e04
Shuffle21 <>
2023-02-04 19:11:56 +0200

    Refactored `t_chat_ui_message`

--- src/types/t_chat_ui_message.ts
+++ src/types/t_chat_ui_message.ts
@@ -1,9 +1,9 @@
+import { t_emotes } from "./t_emotes";
+
 export interface t_chat_ui_message {
     text: string;
     username: string;
     color: string;
-    emotes: {
-        [key: string]: string;
-    };
+    emotes: t_emotes,
     badges: string[];
 }
